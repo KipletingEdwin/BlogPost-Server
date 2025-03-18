03fd70c94cd5ee36e9ba9108d7a31acde17d22a7
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-03-18 11:16:56 +0000

    Update Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
-end
+    before_action :authorize_request
+  
+    private
+  
+    def authorize_request
+      header = request.headers['Authorization']
+      token = header.split(' ').last if header
+      begin
+        decoded = JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
+        @current_user = User.find(decoded[0]['user_id'])
+      rescue ActiveRecord::RecordNotFound, JWT::DecodeError
+        render json: { error: 'Invalid token' }, status: :unauthorized
+      end
+    end
+  end
+  

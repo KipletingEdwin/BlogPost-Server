cec343687aa047cf3a86f9c67ea2932fdefe6119
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-03-18 11:15:16 +0000

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,47 @@
 class PostsController < ApplicationController
-end
+    before_action :authorize_request, except: [:index]
+    before_action :set_post, only: [:update, :destroy]
+  
+    def index
+      posts = Post.includes(:user).all
+      render json: posts, include: :user
+    end
+  
+    def create
+      post = @current_user.posts.create(post_params)
+      if post.save
+        render json: post, status: :created
+      else
+        render json: { error: post.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @post.user == @current_user
+        @post.update(post_params)
+        render json: @post
+      else
+        render json: { error: 'Not authorized' }, status: :forbidden
+      end
+    end
+  
+    def destroy
+      if @post.user == @current_user
+        @post.destroy
+        render json: { message: 'Post deleted' }
+      else
+        render json: { error: 'Not authorized' }, status: :forbidden
+      end
+    end
+  
+    private
+  
+    def set_post
+      @post = Post.find(params[:id])
+    end
+  
+    def post_params
+      params.permit(:title, :content, :image)
+    end
+  end
+  
